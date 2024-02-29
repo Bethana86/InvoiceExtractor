e04ef2b4b6a3e9a74ae9c6ce1c9904b41c80643d
Bethanasamy Rajamani <46414051+Bethana86@users.noreply.github.com>
2024-02-29 09:43:59 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -51,11 +51,3 @@ if submit:
     st.subheader("Response")
     st.write(response)
 
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
